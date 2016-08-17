2387d67b08dc7ab051d002bf25a64b50df68a5b3
mostmark <mostmark@example.com>
2016-08-17 21:12:06 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>This is the best PHP application in the world</h1>
+        <?php
+        echo "Hello World!";
+        ?>
+    </body>
+</html>
