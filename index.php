84e429319354c3cbe5d21c4ea51ff88003f0833e
mostmark <martin@ostmarks.se>
2018-01-26 16:19:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,2 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        <style>
-          .rectangle {
-              width:500px;
-              height:100px;
-              border:0px solid #000;
-              text-align:center;
-              background-color: blue;
-          }
-        </style>
-    </head>
-    <body>
-        <h1>This is the best PHP application in the world!</h1>
-        <br/>
-        <?php
-        echo "The time is <b>" . date("h:i:sa") . "</b>";
-        echo "<br/>The hostname is <b>" . gethostname() . "</b>";
-        ?>
-        <br/>
-        <br/>
-        <div class="rectangle"/>
-    </body>
-</html>
+echo "The time is <b>" . date("h:i:sa") . "</b>";
+echo "<br/>The hostname is <b>" . gethostname() . "</b>";
